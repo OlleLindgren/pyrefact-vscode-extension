747d4ef618171aaae5f53d73db0facee23781212
ollelindgren <lindgrenolle@live.se>
2023-01-03 20:40:51 +0100

    Bump version to 2022.0.5, build dependencies

--- bundled/tool/server.py
+++ bundled/tool/server.py
@@ -52,7 +52,7 @@ RUNNER = pathlib.Path(__file__).parent / "runner.py"
 MAX_WORKERS = 5
 LSP_SERVER = server.LanguageServer(
     name="pyrefact",
-    version="2023.0.4",
+    version="2023.0.5",
     max_workers=MAX_WORKERS,
 )
 
