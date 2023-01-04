ba94225c8700c3412c21e2aea6fcf763e50b9f47
ollelindgren <lindgrenolle@live.se>
2023-01-04 23:32:46 +0100

    Bump pyreact to 58, version to 2023.0.9

--- bundled/tool/server.py
+++ bundled/tool/server.py
@@ -52,7 +52,7 @@ RUNNER = pathlib.Path(__file__).parent / "runner.py"
 MAX_WORKERS = 5
 LSP_SERVER = server.LanguageServer(
     name="pyrefact",
-    version="2023.0.8",
+    version="2023.0.9",
     max_workers=MAX_WORKERS,
 )
 
