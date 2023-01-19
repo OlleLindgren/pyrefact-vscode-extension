aa203be4242740a6a0776e0bad1841fec221c6d5
ollelindgren <lindgrenolle@live.se>
2023-01-19 22:08:59 +0100

    Bump pyrefact to 63, version to 2023.0.13

--- bundled/tool/server.py
+++ bundled/tool/server.py
@@ -52,7 +52,7 @@ RUNNER = pathlib.Path(__file__).parent / "runner.py"
 MAX_WORKERS = 5
 LSP_SERVER = server.LanguageServer(
     name="pyrefact",
-    version="2023.0.12",
+    version="2023.0.13",
     max_workers=MAX_WORKERS,
 )
 
