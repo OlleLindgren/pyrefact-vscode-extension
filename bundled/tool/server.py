6495eea62553b5f7b54a232dd38a8e9e986b050d
ollelindgren <lindgrenolle@live.se>
2023-03-03 19:04:54 +0100

    Bump version to 2023.0.22
    Several dependencies are updated. Pyrefact is not updated.

--- bundled/tool/server.py
+++ bundled/tool/server.py
@@ -53,7 +53,7 @@ RUNNER = pathlib.Path(__file__).parent / "runner.py"
 MAX_WORKERS = 5
 LSP_SERVER = server.LanguageServer(
     name="pyrefact",
-    version="2023.0.21",
+    version="2023.0.22",
     max_workers=MAX_WORKERS,
 )
 
