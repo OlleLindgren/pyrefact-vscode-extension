a81eecbb6b9024a928fa3cdcf3328b76d14eec69
ollelindgren <lindgrenolle@live.se>
2023-04-29 14:42:04 +0200

    Fix test for new pyrefact version

--- src/test/python_tests/test_data/sample1/sample.py
+++ src/test/python_tests/test_data/sample1/sample.py
@@ -1,11 +1,10 @@
 def asdf():
 
-    if 2 in {1, 2, 3}:
-        print(3)
+    print(3)
+
+
 class Foo:
     @staticmethod
     def asdf():
 
-        if 2 in {1, 2, 3}:
-
-            print(3)
+        print(3)
